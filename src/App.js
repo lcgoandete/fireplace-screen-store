99a3e7b4628ec4775a0c03c596fca531824c47ff
lcgoandete <lcgoandete@hotmail.com>
2021-03-28 14:57:34 -0300

    criacao dos componentes header e footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Product from './pages/Product';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Product />
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logoPequeno from '../images/logo_pequeno.jpeg'
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <img src={ logoPequeno } alt="logotipo" />
+      </header>
+    );
+  }
+}
+
+export default Header;
